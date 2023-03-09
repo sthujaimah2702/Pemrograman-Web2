1d85dd4574cf4dca2d4fd3ba634a3b3c54757916
sthujaimah2702 <siti22136si@student.nurulfikri.ac.id>
2023-03-09 15:31:09 +0700

    Upload Praktikum 3

---
+++ Praktikum03/proses_registrasi.php
@@ -0,0 +1,20 @@
+<?php 
+// array numerik
+$domisili = ["Jakarta", "Bogor", "Depok", "Tangerang", "Bekasi"];
+// array asosiatif (indeksnya bisa kita ubah sendiri)
+$program_studi =[
+    "SI"=>"Sistem Informasi", 
+    "TI"=>"Teknik Informatika", 
+    "BD"=>"Bisnis Digital"];
+
+$skills = [
+    "HTML"=>10,
+    "CSS"=>10,
+    "Javascript"=>20,
+    "RWD Bootstrap"=>20,
+    "PHP"=>30,
+    "Python"=>30,
+    "Java"=>50
+];
+
+?>
